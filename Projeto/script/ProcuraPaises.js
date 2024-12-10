6b8e0de72788a7ecee0828e33248ee612268786b
MrMagmaPT <pmrmagma@gmail.com>
2024-12-10 18:54:02 +0000

    Agora feito com o ajax como a stora queria

--- Projeto/script/ProcuraPaises.js
+++ Projeto/script/ProcuraPaises.js
@@ -1,60 +1,30 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const countriesContainer = document.getElementById('countries');
-    let countriesData = [];
+    var cloneOriginalCard = $('.card-countries').clone();
 
-    // Função para criar um card de país
-    function createCountryCard(country, countryName) {
-        const countryCard = document.createElement('div');
-        countryCard.className = 'col-md-4';
-        countryCard.innerHTML = `
-            <div class="card">
-                <img src="${country.flags.png}" class="card-img-top" alt="${countryName}">
-                <div class="card-body">
-                    <h5 class="card-title">${countryName}</h5>
-                </div>
-            </div>
-        `;
-        countryCard.addEventListener('click', () => {
-            window.location.href = `detalhes.html?name=${countryName}`;
-        });
-        return countryCard;
-    }
 
-    // Função para exibir os países
-    function displayCountries(countries) {
-        countriesContainer.innerHTML = '';  // Limpar os cards existentes
-        countries.forEach(country => {
-            const countryName = country.name.common;
-            const countryCard = createCountryCard(country, countryName);
-            countriesContainer.appendChild(countryCard);
-        });
-    }
+$('#btn-search').on('click', function(){
+    var valorPesquisa = $('#pais_nome').val();
 
-    // Função para fazer a chamada AJAX
-    function BuscarPaises() {
-        const xhr = new XMLHttpRequest();
-        xhr.open("GET", "https://restcountries.com/v3.1/all", true);
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState === 4 && xhr.status === 200) {
-                countriesData = JSON.parse(xhr.responseText);
-                displayCountries(countriesData);
-            }
-        };
-        xhr.send();
-    }
+    $('#countries').html('');
 
-    // Chamar a função para buscar os países
-    BuscarPaises();
 
-    // Adicionar evento de pesquisa
-    const searchInput = document.getElementById('search');
-    searchInput.addEventListener('input', function() {
-        const searchTerm = searchInput.value.toLowerCase();
-        const filteredCountries = countriesData.filter(country => {
-            const countryName = country.name.common;
-            return countryName.toLowerCase().includes(searchTerm);
-        });
-        displayCountries(filteredCountries);
+
+    $.ajax({
+        method:'GET',
+        url:'https://www.omdbapi.com/?i=tt3896198&apikey=f8242fe8&s=' + valorPesquisa
+    }).done(function(dados){
+        console.log(dados);
+
+        console.log(dados.totalResults);
+
+        for(var i=0; i< dados.Search.length;i++) {
+            console.log(dados.Search[i].Title);
+            var clonecard = cloneOriginalCard.clone();
+            
+            $('.card-img-top',clonecard).attr('src', dados.Search[i].Poster);
+            $('.card-title',clonecard).text(dados.Search[i].Title);
+            $('#countries').append(clonecard)
+        }
+
+
     });
-});
-
+});  
