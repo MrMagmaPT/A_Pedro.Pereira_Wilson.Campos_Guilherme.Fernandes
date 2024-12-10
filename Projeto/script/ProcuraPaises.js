8c41912785246a8429d60ed6c59827604d71221b
MrMagmaPT <pmrmagma@gmail.com>
2024-12-10 22:32:07 +0000

    atualizei os paiss para a rest api ta a funcionar quase perfeitamente so falta mudar o card em si

--- Projeto/script/ProcuraPaises.js
+++ Projeto/script/ProcuraPaises.js
@@ -1,30 +1,30 @@
-    var cloneOriginalCard = $('.card-countries').clone();
-
+var cloneOriginalCard = $('.card-countries').clone();
 
 $('#btn-search').on('click', function(){
     var valorPesquisa = $('#pais_nome').val();
+    var valorPesquisaSufix= "name/"
+    if (valorPesquisa == "") {
+        valorPesquisaSufix = "all/";
+    }
 
     $('#countries').html('');
 
-
-
     $.ajax({
         method:'GET',
-        url:'https://www.omdbapi.com/?i=tt3896198&apikey=f8242fe8&s=' + valorPesquisa
+        url:'https://restcountries.com/v3.1/' + valorPesquisaSufix + valorPesquisa
     }).done(function(dados){
         console.log(dados);
 
-        console.log(dados.totalResults);
-
-        for(var i=0; i< dados.Search.length;i++) {
-            console.log(dados.Search[i].Title);
-            var clonecard = cloneOriginalCard.clone();
+        dados.forEach(element =>{
+            console.log(element.name.common);
+            var cloneCard = cloneOriginalCard.clone();
             
-            $('.card-img-top',clonecard).attr('src', dados.Search[i].Poster);
-            $('.card-title',clonecard).text(dados.Search[i].Title);
-            $('#countries').append(clonecard)
-        }
+            $('.card-img-top',cloneCard).attr('src', element.flags.png);
+            $('.card-title',cloneCard).text(element.name.common);
+            $('#countries').append(cloneCard)
+        });
 
 
+        //for(var i=0; i< dados.Search.length;i++) {}
     });
 });  
